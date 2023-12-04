5496185621ec5df6c1b27277e9f5d0333b7e8337
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-12-04 16:14:10 -0500

    Day 3 Puzzle 1

---
+++ Day3/Puzzle1/Program.cs
@@ -0,0 +1,56 @@
+﻿// var file =  "467..114.. ...*...... ..35..633. ......#... 617*...... .....+.58. ..592..... ......755. ...$.*.... .664.598..".Split(" ").ToArray();
+
+var file = File.ReadAllLines("../aoc_day_3");
+
+var width = file[0].Length;
+var height = file.Length;
+
+(int, int)[] directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)];
+
+var map = new char[width, height];
+
+for (var x = 0; x < width; x++)
+{
+    for (var y = 0; y < height; y++)
+    {
+        map[x, y] = file[y][x];
+    }
+}
+
+var total = 0;
+var currentNumber = 0;
+var isAdjacent = false;
+
+for (var i = 0; i < height; i++)
+{
+    for (var j = 0; j < height; j++)
+    {
+        var character = map[j, i];
+
+        if (char.IsDigit(character))
+        {
+            var value = character - '0';
+            currentNumber = currentNumber * 10 + value;
+            foreach (var direction in directions)
+            {
+                var neigbhorJ = j + direction.Item1;
+                var neigbhorI = i + direction.Item2;
+                if (neigbhorJ < 0 || neigbhorJ >= width || neigbhorI < 0 || neigbhorI >= height)
+                    continue;
+                var neighborCharacter = map[neigbhorJ, neigbhorI];
+                isAdjacent = !char.IsDigit(neighborCharacter) && neighborCharacter != '.' ? true : isAdjacent;
+            }
+        }
+        else 
+        {
+            total = currentNumber != 0 && isAdjacent ? total + currentNumber : total;
+            currentNumber = 0;
+            isAdjacent = false;
+        }
+    }
+    total = currentNumber != 0 && isAdjacent ? total + currentNumber : total;
+    currentNumber = 0;
+    isAdjacent = false;
+}
+
+Console.WriteLine($"Total: {total}");
