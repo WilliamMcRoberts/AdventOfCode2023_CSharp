8a9024893f33bdaf09fd052b93e4568d5cbfb02e
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-12-13 18:06:26 -0500

    Day 11 Puzzle 1

---
+++ Day10/Puzzle1/Program.cs
@@ -0,0 +1,128 @@
+﻿var file = File.ReadAllLines("../aoc_day_10");
+
+var list = file.Select(x =>
+{
+    Console.WriteLine(x);
+    return x;
+}).ToList();
+
+
+var maxHeap = new PriorityQueue<int>();
+
+maxHeap.Enqueue(5);
+maxHeap.Enqueue(4);
+maxHeap.Enqueue(3);
+maxHeap.Enqueue(2);
+maxHeap.Enqueue(9);
+maxHeap.Enqueue(8);
+maxHeap.Enqueue(7);
+maxHeap.Enqueue(6);
+
+maxHeap.Enqueue(1);
+
+// Get the peak element (i.e., the smallest element)
+int peakElement = maxHeap.Peek();
+
+// Print the peak element
+Console.WriteLine("Max Peak element: " + peakElement);
+
+
+var minHeap = new PriorityQueue<int>(false);
+
+minHeap.Enqueue(5);
+minHeap.Enqueue(4);
+minHeap.Enqueue(3);
+minHeap.Enqueue(20);
+minHeap.Enqueue(380);
+
+Console.WriteLine("Min Peak element: " + minHeap.Peek());
+
+
+string t = string.Empty;
+
+t = "YOOOOOO";
+
+Console.WriteLine(t);
+
+class PriorityQueue<T>(bool isMaxHeap = true) where T : IComparable<T>
+{
+    private List<T> heap = [];
+
+
+    public int Count => this.heap.Count;
+
+    public void Enqueue(T item)
+    {
+        this.heap.Add(item);
+        this.BubbleUp(this.heap.Count - 1);
+    }
+
+    public T Dequeue()
+    {
+        T item = this.heap[0];
+        int lastIndex = this.heap.Count - 1;
+        this.heap[0] = this.heap[lastIndex];
+        this.heap.RemoveAt(lastIndex);
+        this.BubbleDown(0);
+        return item;
+    }
+
+    public T Peek() => this.heap[0];
+
+    private void BubbleUp(int index)
+    {
+        while (index > 0)
+        {
+            int parentIndex = (index - 1) / 2;
+            if (isMaxHeap)
+            {
+                if (this.heap[parentIndex].CompareTo(this.heap[index]) >= 0)
+                    break;
+            }
+            else
+            {
+                if (this.heap[parentIndex].CompareTo(this.heap[index]) <= 0)
+                    break;
+            }
+            Swap(parentIndex, index);
+            index = parentIndex;
+        }
+    }
+
+    private void BubbleDown(int index)
+    {
+        while (index < this.heap.Count)
+        {
+            int leftChildIndex = index * 2 + 1;
+            int rightChildIndex = index * 2 + 2;
+            if (!isMaxHeap)
+            {
+                int smallestchildIndex = index;
+                if (leftChildIndex < this.heap.Count && this.heap[leftChildIndex].CompareTo(this.heap[smallestchildIndex]) < 0)
+                    smallestchildIndex = leftChildIndex;
+                if (rightChildIndex < this.heap.Count && this.heap[rightChildIndex].CompareTo(this.heap[smallestchildIndex]) < 0)
+                    smallestchildIndex = rightChildIndex;
+                if (smallestchildIndex == index)
+                    break;
+
+                Swap(smallestchildIndex, index);
+                index = smallestchildIndex;
+                return;
+            }
+
+            int largestChildIndex = index;
+            if (leftChildIndex < this.heap.Count && this.heap[leftChildIndex].CompareTo(this.heap[largestChildIndex]) > 0)
+                largestChildIndex = leftChildIndex;
+            if (rightChildIndex < this.heap.Count && this.heap[rightChildIndex].CompareTo(this.heap[largestChildIndex]) > 0)
+                largestChildIndex = rightChildIndex;
+            if (largestChildIndex == index)
+                break;
+
+            Swap(largestChildIndex, index);
+            index = largestChildIndex;
+        }
+    }
+
+    private void Swap(int i, int j) =>
+        (this.heap[j], this.heap[i]) = (this.heap[i], this.heap[j]);
+}
---
+++ Day11/Puzzle1/Program.cs
@@ -0,0 +1,68 @@
+﻿var lines = File.ReadAllLines("../aoc_day_11").ToList();
+
+long SumOfGalaxyDistances(List<string> lines, int expansion)
+{
+    // Expand rows
+    List<int> rowExpansions = new();
+    for (var i = 0; i < lines.Count; i++)
+    {
+        if (lines[i].All(x => x == '.')) 
+            rowExpansions.Add(i);
+    }
+    
+    // Expand columns
+    List<int> columnExpansions = new();
+    for (var i = 0; i < lines[0].Length; i++)
+    {
+        var foundGalaxy = false;
+        foreach (var line in lines)
+        {
+            if (line[i] == '#')
+                foundGalaxy = true;
+        }
+    
+        if (!foundGalaxy) 
+            columnExpansions.Add(i);
+    }
+    
+    // Map galaxies
+    List<(int y, int x)> galaxies = new();
+    for (var y = 0; y < lines.Count; y++)
+    {
+        for (var x = 0; x < lines[0].Length; x++)
+        {
+            if (lines[y][x] == '#')
+            {
+                int yExpansion;
+                for (yExpansion = 0; yExpansion < rowExpansions.Count; yExpansion++)
+                {
+                    if (rowExpansions[yExpansion] > y) break;
+                }
+                
+                int xExpansion;
+                for (xExpansion = 0; xExpansion < columnExpansions.Count; xExpansion++)
+                {
+                    if (columnExpansions[xExpansion] > x) break;
+                }
+
+                galaxies.Add((y + yExpansion * expansion, x + xExpansion * expansion));
+            }
+        }
+    }
+    
+    int sum = 0;
+    foreach (var (y1, x1) in galaxies)
+    {
+        foreach (var (y2, x2) in galaxies)
+        {
+            sum += Math.Abs(y1 - y2) + Math.Abs(x1 - x2);
+        }
+    }
+
+    // ???
+    return sum / 2;
+}
+
+var sum = SumOfGalaxyDistances(lines, 1);
+Console.WriteLine($"Sum: {sum}");
+
