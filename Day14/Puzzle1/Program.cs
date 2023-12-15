65d4e63621430ae1e107bb35b96fbbe24534c113
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-12-15 16:05:57 -0500

    Day 14 Puzzle 1

---
+++ Day14/Puzzle1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+char[][] input = File.ReadAllLines("../aoc_day_14").Select(x => x.ToCharArray()).ToArray(); 
+
+for (int i = 1; i < input.Length; i++)
+{
+  for(int j = 0; j < input[i].Length; j++)
+  {
+    if (input[i][j] != 'O')
+					continue;     
+
+    for (var k = i; k > 0 && input[k - 1][j] == '.'; k--)
+				{
+          (input[k - 1][j], input[k][j]) = (input[k][j], input[k - 1][j]);
+				}
+  }
+}
+
+int total = 0;
+for(int i = 0; i < input.Length; i++)
+{
+  total += (input[i].Count(x => x == 'O') * (input.Length - i));
+}
+
+Console.WriteLine($"Total: {total}");
